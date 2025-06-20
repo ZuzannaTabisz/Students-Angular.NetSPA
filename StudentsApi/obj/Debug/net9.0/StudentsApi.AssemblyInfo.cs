e6b8b07c0f380c08163df9d6b5b0f4b9ac455fce
ZuzannaTabisz <zuzannatabisz.zt@gmail.com>
2025-06-20 23:40:14 +0200

    add

--- StudentsApi/obj/Debug/net9.0/StudentsApi.AssemblyInfo.cs
+++ StudentsApi/obj/Debug/net9.0/StudentsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b41269d69ae18de3b858ad9e43970ac00a58016")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
